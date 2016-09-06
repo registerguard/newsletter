6cca0fa4d68c3601db6d5114616802df317846f0
Rob Denton <robdentonrg@gmail.com>
2016-09-06 15:44:26 -0700

    move functions into shared file yay!

--- duck.py
+++ duck.py
@@ -5,147 +5,39 @@
 #            !!! DANGER !!!
 #
 # This script will send an email to all recipients of campaign
-# To test the email, use the test_email.py file
+#
+# Comment out sendEmail() and uncomment sendTest() to test
+#
 
-import requests # Need to install
-import json, datetime
-from config import MailChimpConfig
+import datetime
+from shared import createCampaign, setContent, sendTest, sendEmail
 
 today = datetime.date.today()
 date = "{:%x}".format(today)
 
-config = MailChimpConfig()
-# print campaign_id
+# Vars for campaign creation
+#list = "" # Safety
+list = "da06b9d3a2" # Duck News Daily list
+subject = 'Duck News Daily: *|DATE:l, F j, Y|*'
+title = 'Duck News Daily: {0}'.format(date)
+folder = '8a260761a0'
 
-def createCampaign(key):
-	path = "campaigns"
-	endpoint = config.api_root + path
-	
-	meta = {}
-	
-	meta['recipients'] = {
-		#'segment_opts': {
-		#	'conditions': [{
-				### Send to Rob
-				#'field': 'EMAIL',
-				#'condition_type': 'EmailAddress',
-				#'value': 'rob.denton',
-				#'op': 'contains'
-				###################################
-				### Send to Test group
-				#'field': 'interests-3fcd22fb3e',
-				#'condition_type': 'Interests',
-				#'value': ['a51e8d83ce'],
-				#'op': 'interestcontains'
-				###################################
-				### REAL list
-		#		'field':'interests-c7a0ec6b55',
-		#		'condition_type':'Interests',
-		#		'value':[ 'f85f0f3bb3' ],
-		#		'op':'interestcontains'
-		#	}],
-		#	'match': 'any'
-		#},
-		### Test list
-		#'list_id': '824c7efd1d'
-		### REAL list
-		#'list_id':'d53bd3b699'
-		###
-		### NEW Duck News Daily list
-		###
-		'list_id': 'da06b9d3a2'
-	}
-	
-	meta['settings'] = {
-		'subject_line': 'Duck News Daily: *|DATE:l, F j, Y|*',
-		'from_name': 'The Register-Guard',
-		'title': 'Duck News Daily: {0}'.format(date),
-		'inline_css': True,
-		'fb_comments': False,
-		'auto_footer': False,
-		'athenticate': True,
-		'to_name': '*|FNAME|* *|LNAME|*',
-		'folder_id': '8a260761a0',
-		'reply_to': 'promotions@registerguard.com',
-		'auto_tweet': False,
-	}
-	
-	meta['type'] = 'regular'
-	
-	#print meta
-	
-	payload = json.dumps(meta)
-	
-	#print payload
-	
-	response = requests.post(endpoint, auth=('apikey', key), data=payload)
-	
-	#print response.json()
-	
-	try:
-		response.raise_for_status()
-		body = response.json()
-		id = body['id']
-		return id
-	except requests.exceptions.HTTPError as err:
-		print '\n\n\nError: %s' % err
-	
-	
+# Create campaign and get ID
+id = createCampaign(list, subject, title, folder)
+#id = "ea9d94e671" # For testing (use last created campaign ID)
+print id
 
-def setContent(key, id):
-	content = "campaigns/{0}/content".format(id)
-	endpoint = config.api_root + content
-	
-	payload = json.dumps({"url": "http://registerguard.com/csp/cms/rg/pages/newsletters/football.csp"})
-	
-	response = requests.put(endpoint, auth=('apikey', key), data=payload)
-	
-	try:
-		response.raise_for_status()
-	except requests.exceptions.HTTPError as err:
-		print '\n\n\nError: %s' % err
-		print response.json()
+# Vars for setting content
+url = "http://registerguard.com/csp/cms/rg/pages/newsletters/football.csp"
 
-def sendTest(key, id):
-	test = "campaigns/{0}/actions/test".format(id)
-	endpoint = config.api_root + test
-	
-	payload = json.dumps({"test_emails":["robdentonrg@gmail.com"],"send_type":"html"})
-	
-	#print  "\nPayload: " + payload
-	
-	response = requests.post(endpoint, auth=('apikey', key), data=payload)
-	#print response.json()
-	
-	#print "\nURL: " + response.url + "\n\n"
-	
-	try:
-		response.raise_for_status()
-		print "\n\nTEST SENT!!!\n\n"
-	except requests.exceptions.HTTPError as err:
-		print '\n\n\nError: %s' % err
-	
+# Set content
+setContent(id, url)
 
-def sendEmail(key, id):
-	email = "campaigns/{0}/actions/send".format(id)
-	endpoint = config.api_root + email
-	
-	response = requests.post(endpoint, auth=('apikey', key))
-	
-	#print "\nURL: " + response.url + "\n\n"
-	
-	try:
-		response.raise_for_status()
-		#print "\n\nCAMPAIGN SENT!!!\n\n"
-	except requests.exceptions.HTTPError as err:
-		print '\n\n\nError: %s' % err
-		print response.json()
+# Vars for test email
+test_emails = ["robdentonrg@gmail.com"]
 
+#Send test (Normally commented out)
+#sendTest(id, test_emails)
 
 # Call method
-campaign_id = createCampaign(config.apikey)
-#campaign_id = "ea9d94e671" # For testing (use last created campaign ID)
-#print campaign_id
-setContent(config.apikey, campaign_id)
-#sendTest(config.apikey, campaign_id)
-sendEmail(config.apikey, campaign_id)
+sendEmail(id)
---
+++ shared.py
@@ -0,0 +1,117 @@
+import requests, json
+from config import MailChimpConfig
+
+config = MailChimpConfig()
+
+def createCampaign(list, subject, title, folder):
+    path = "campaigns"
+    endpoint = config.api_root + path
+    
+    meta = {}
+    
+    meta['recipients'] = {
+#       'segment_opts': { # This stuff only for testing
+#           'conditions': [{
+#               ### Send to Rob
+#               'field': 'EMAIL',
+#               'condition_type': 'EmailAddress',
+#               'value': 'rob.denton',
+#               'op': 'contains'
+#               ###################################
+#               ### Send to Test group
+#               #'field': 'interests-3fcd22fb3e',
+#               #'condition_type': 'Interests',
+#               #'value': ['a51e8d83ce'],
+#               #'op': 'interestcontains'
+#           }],
+#           'match': 'any'
+#       },
+#       ### Test list
+#       'list_id': '824c7efd1d'
+        'list_id': list
+    }
+    
+    meta['settings'] = {
+        'subject_line': subject,
+        'from_name': 'The Register-Guard',
+        'title': title,
+        'inline_css': True,
+        'fb_comments': False,
+        'auto_footer': False,
+        'athenticate': True,
+        'to_name': '*|FNAME|* *|LNAME|*',
+        'folder_id': folder,
+        'reply_to': 'promotions@registerguard.com',
+        'auto_tweet': False,
+    }
+    
+    meta['type'] = 'regular'
+    
+    #print meta
+    
+    payload = json.dumps(meta)
+    
+    #print payload
+    
+    response = requests.post(endpoint, auth=('apikey', config.apikey), data=payload)
+    
+    #print response.json()
+    
+    try:
+        response.raise_for_status()
+        body = response.json()
+        id = body['id']
+        return id
+    except requests.exceptions.HTTPError as err:
+        print '\n\n\nError: %s' % err
+
+def setContent(id, url):
+    content = "campaigns/{0}/content".format(id)
+    endpoint = config.api_root + content
+    
+    payload = json.dumps({"url": url})
+    
+    response = requests.put(endpoint, auth=('apikey', config.apikey), data=payload)
+    
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print '\n\n\nError: %s' % err
+        print response.json()
+
+def sendTest(id, test_emails):
+    test = "campaigns/{0}/actions/test".format(id)
+    endpoint = config.api_root + test
+    
+    payload = json.dumps({"test_emails": test_emails,"send_type":"html"})
+    
+    #print  "\nPayload: " + payload
+    
+    response = requests.post(endpoint, auth=('apikey', config.apikey), data=payload)
+    #print response.json()
+    
+    #print "\nURL: " + response.url + "\n\n"
+    
+    try:
+        response.raise_for_status()
+        print "\n\nTEST SENT!!!\n\n"
+    except requests.exceptions.HTTPError as err:
+        print '\n\n\nError: %s' % err
+        
+    
+
+def sendEmail(id):
+    email = "campaigns/{0}/actions/send".format(id)
+    endpoint = config.api_root + email
+    
+    response = requests.post(endpoint, auth=('apikey', config.apikey))
+    
+    #print "\nURL: " + response.url + "\n\n"
+    
+    try:
+        response.raise_for_status()
+        #print "\n\nCAMPAIGN SENT!!!\n\n"
+    except requests.exceptions.HTTPError as err:
+        print '\n\n\nError: %s' % err
+        print response.json()
+        
